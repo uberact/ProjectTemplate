fc079b0cbf981e65af7cade743920ea45fc8ba9b
gngriffiths <gareth.griffiths@uberact.com>
2016-04-20 17:47:54 +0100

    Minor update to _launch.bat correcting the vvvv folder name

--- programfiles/_launch.bat
+++ programfiles/_launch.bat
@@ -1,1 +1,1 @@
-start ..\Libraries\vvvv_beta34.2_x64\vvvv.exe /o "..\..\Programfiles\_roota.v4p" /allowmultiple
+start ..\Libraries\vvvv_45beta34.2_x64\vvvv.exe /o "..\..\Programfiles\_roota.v4p" /allowmultiple
